b71a818cae2425699413cdaa637ca2ff0f5ed956
hiro <motoichikakaka@gmail.com>
2022-08-30 23:20:18 +0900

    認証機能の実装完了

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
     namespace :api do
       namespace :v1 do
         resources :test, only: %i[index]
-        # get '/users', to: 'users#index'
-        # get '/users/:id/edit', to: 'users#edit'
-        # get '/users/:id', to: 'users#update'
-        # delete '/users/:id', to: 'users#destroy'
 
-        # authに関するルーティングを自動生成
         mount_devise_token_auth_for 'User', at: 'auth', controllers: {
           registrations: 'api/v1/auth/registrations'
         }
+
+        get 'users', to: 'users#index'
+        get 'users/:id/edit', to: 'users#edit'
+        patch 'users/:id', to: 'users#update'
+        delete 'users/:id', to: 'users#destroy'
   
         namespace :auth do
           resources :sessions, only: %i[index]
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,11 +1,11 @@
-User.create!(name: "元博",
-             email: "motohiro@example.org",
-             password: "shimo090da")
+# User.create!(name: "元博",
+#              email: "motohiro@example.org",
+#              password: "shimo090da")
 
-User.create!(name: "せいじ",
-             email: "seiji@example.org",
-             password: "foobaz")
+# User.create!(name: "せいじ",
+#              email: "seiji@example.org",
+#              password: "foobaz")
 
-User.create!(name: "幹太",
-             email: "kanta@example.org",
-             password: "foobay")
+# User.create!(name: "幹太",
+#              email: "kanta@example.org",
+#              password: "foobay")
